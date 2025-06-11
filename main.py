0ce642e365f5bff53908cd458dbcff074543c043
Kunal <kunal.sahni1999@gmail.com>
2025-06-12 00:48:01 +0530

    Setting Path Parameters and Type Hints

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from enum import Enum
 
 app = FastAPI()
+
+class GenreURLChoices(Enum):
+    ROCK = 'rock'
+    ELECTRONIC = 'electronic'
+    METAL = 'metal'
+    HIP_HOP = 'hip-hop'
+
+BANDS = [
+    {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
+    {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
+    {'id': 3, 'name': 'Black Sabbath', 'genre': 'Metal'},
+    {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
+]
 
 @app.get('/')
 async def index() -> dict[str, str]:
@@ -9,3 +23,20 @@ async def index() -> dict[str, str]:
 @app.get('/about')
 async def about() -> str:
     return "About page of the company ABC"
+
+@app.get('/bands')
+async def bands() -> list[dict]:
+    return BANDS
+
+@app.get('/bands/{band_id}')
+async def band_detail(band_id: int) -> dict:
+    band = next((b for b in BANDS if b['id'] == band_id), None)
+    if band == None:
+        raise HTTPException(status_code=404, detail='Band not found')
+    return band
+
+@app.get('/bands/genre/{genre}')
+async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:
+    return [
+        b for b in BANDS if b['genre'].lower() == genre.value
+    ]
