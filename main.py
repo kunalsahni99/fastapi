fd0f278be6aa016f1bcfeca518e653c7b30f99b9
Kunal <kunal.sahni1999@gmail.com>
2025-06-16 20:32:07 +0530

    Adding pydantic models

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI, HTTPException
-from enum import Enum
+from schemas import Band, GenreURLChoices
 
 app = FastAPI()
-
-class GenreURLChoices(Enum):
-    ROCK = 'rock'
-    ELECTRONIC = 'electronic'
-    METAL = 'metal'
-    HIP_HOP = 'hip-hop'
 
 BANDS = [
     {'id': 1, 'name': 'The Kinks', 'genre': 'Rock'},
     {'id': 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
-    {'id': 3, 'name': 'Black Sabbath', 'genre': 'Metal'},
+    {'id': 3, 'name': 'Black Sabbath', 'genre': 'Metal', 'albums': [
+        {'title': 'Master of Reality', 'release_date': '1971-07-21'}
+    ]},
     {'id': 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},
 ]
 
@@ -25,12 +21,14 @@ async def about() -> str:
     return "About page of the company ABC"
 
 @app.get('/bands')
-async def bands() -> list[dict]:
-    return BANDS
+async def bands() -> list[Band]:
+    return [
+        Band(**b) for b in BANDS
+    ]
 
 @app.get('/bands/{band_id}')
-async def band_detail(band_id: int) -> dict:
-    band = next((b for b in BANDS if b['id'] == band_id), None)
+async def band_detail(band_id: int) -> Band:
+    band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
     if band == None:
         raise HTTPException(status_code=404, detail='Band not found')
     return band
